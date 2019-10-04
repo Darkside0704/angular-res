2cb92dee5a23cc055c6cdd58eb9b1951b125a88b
Darkside0704 <williamtupan89@gmail.com>
2019-10-03 20:04:38 -0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Gita';
+  arrayAngka = ['siji','loro','telu']
+  hasil:number;
+  temp:string;
+  x="";
+  y="";
+  appComponent(){
+
+  }
+  add(){
+    if(this.x != "" && this.y != "")
+    this.hasil = +this.x + +this.y;
+    this.temp = this.hasil+""
+    }
+  min(){
+    if(this.x != "" && this.y != "")
+    this.hasil = +this.x - +this.y;
+    this.temp = this.hasil+""
+  }
+  mul(){
+    if(this.x != "" && this.y != "")
+    this.hasil = +this.x * +this.y;
+     this.temp = this.hasil+""
+  }
+  bag(){
+    if(this.x != "" && this.y != "" && this.y !="0")
+    this.hasil = +this.x / +this.y;
+    this.temp = this.hasil+""
+    if(this.y == "0")
+    this.temp = "Error" ;
+    }
 }
